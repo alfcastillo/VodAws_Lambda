87e5ae10b2e95a28bda33add64451105a3474e5b
EC2 Default User <ec2-user@ip-172-31-46-222.ec2.internal>
2021-10-27 16:01:55 +0000

    initial commit

---
+++ src/aws-video-exports.js
@@ -0,0 +1,7 @@
+const awsvideoconfig = {
+			"awsInputVideo": "vodawsstack-rs3inputbucket-ztf1dqc8up-inputbucket-15p038exfmxyg",
+			"awsOutputVideo": "d2dbfbzcb0aasg.cloudfront.net"
+		};
+
+export default awsvideoconfig;
+
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,84 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateVodAsset = /* GraphQL */ `
+  subscription OnCreateVodAsset {
+    onCreateVodAsset {
+      id
+      title
+      description
+      video {
+        id
+        token
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateVodAsset = /* GraphQL */ `
+  subscription OnUpdateVodAsset {
+    onUpdateVodAsset {
+      id
+      title
+      description
+      video {
+        id
+        token
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteVodAsset = /* GraphQL */ `
+  subscription OnDeleteVodAsset {
+    onDeleteVodAsset {
+      id
+      title
+      description
+      video {
+        id
+        token
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateVideoObject = /* GraphQL */ `
+  subscription OnCreateVideoObject {
+    onCreateVideoObject {
+      id
+      token
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateVideoObject = /* GraphQL */ `
+  subscription OnUpdateVideoObject {
+    onUpdateVideoObject {
+      id
+      token
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteVideoObject = /* GraphQL */ `
+  subscription OnDeleteVideoObject {
+    onDeleteVideoObject {
+      id
+      token
+      createdAt
+      updatedAt
+    }
+  }
+`;
